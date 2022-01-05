0735134ffa54b6fa34ea6a6eacf004675535c3a0
Antho Codeur <antho.codeur@gmail.com>
2022-01-05 16:28:48 +0100

    Add Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Modifier <code>src/App.js</code> and enrgistrer pour recharger.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Apprendre React
-        </a>
-      </header>
-    </div>
+    <h1 className="text-3xl text-red-500">
+      Hello world!
+    </h1>
   );
 }
 
